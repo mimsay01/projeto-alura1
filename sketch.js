e7b1032e07049ce427553030565809096e96dcea
mimsay01 <yasmimandressavetroniflorindo@gmail.com>
2024-08-11 20:10:45 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+    background("deeppink");
+  }
+  function draw(){
+  stroke("darkviolet");
+  fill("violet");
+    
+                  
+  //console.log(mouseIsPressed){
+    if (mouseIsPressed){rect(mouseX,mouseY,20,35);
+  }
+    
+  }
